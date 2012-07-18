0085184fa5cb8908496e7dd99c43fd8c093b4f04
moliver777 <moliver@i-neda.com>
2012-07-18 11:30:20 +0100

    user authentication fixes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,18 +3,28 @@ class SessionController < ApplicationController
   skip_before_filter :authenticated_user?
 
   def login
+    @error = session[:error] if session[:error]
+    render :layout => "login"
   end
 
   def create
     user = User.where(:username => params[:username]).first
     if user
-      session[:username] = user.username if params[:password] == user.password
+      if params[:password] == user.password
+        session[:username] = user.username
+        session[:error] = nil
+      else
+        session[:error] = "Incorrect username and/or password"
+      end
+    else
+      session[:error] = "Incorrect username and/or password"
     end
     redirect_to root_path
   end
 
   def destroy
     session[:username] = nil
+    session[:error] = nil
     redirect_to root_path
   end
 end
