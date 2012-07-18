df144bcbf9c285a056d69f335a8348bc4505574f
moliver777 <moliver@i-neda.com>
2012-07-18 09:13:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RycroftSM::Application.config.secret_token = '700e942bda1f323470c9430e4978525b9c57ad9ad5b18e10d667491f37f0c1bacca935ec712da2cde9f0113c6d297734ba82e0c47b7d472b8fb02efcf5ad0242'
