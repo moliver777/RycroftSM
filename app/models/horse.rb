1ef4939979fbb6125d1c28e9a41ab34fa63aa5a1
moliver777 <moliver@i-neda.com>
2012-07-22 20:37:17 +0100

    horse date functions

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -6,4 +6,44 @@ class Horse < ActiveRecord::Base
 
   has_many :bookings
   has_many :notes
+
+  def farrier_due
+    if self.farrier
+      date = self.farrier_date
+      while date < Time.now.to_date
+        date = date.to_time.advance(:days => self.farrier_freq.to_i).to_date
+      end
+    end
+    date ? date.strftime("%Y-%m-%d") : "N/A"
+  end
+
+  def worming_due
+    if self.worming
+      date = self.worming_date
+      while date < Time.now.to_date
+        date = date.to_time.advance(:days => self.worming_freq.to_i).to_date
+      end
+    end
+    date ? date.strftime("%Y-%m-%d") : "N/A"
+  end
+
+  def vet_due
+    if self.vet
+      date = self.vet_date
+      while date < Time.now.to_date
+        date = date.to_time.advance(:days => self.vet_freq.to_i).to_date
+      end
+    end
+    date ? date.strftime("%Y-%m-%d") : "N/A"
+  end
+
+  def medication_due
+    if self.medication
+      date = self.medication_date
+      while date < Time.now.to_date
+        date = date.to_time.advance(:days => self.medication_freq.to_i).to_date
+      end
+    end
+    date ? date.strftime("%Y-%m-%d") : "N/A"
+  end
 end
