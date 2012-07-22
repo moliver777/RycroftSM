bd257657b288ded8a154e8daa1be1fd4a12624cf
moliver777 <moliver@i-neda.com>
2012-07-22 20:51:30 +0100

    work on horse management

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,39 +1,38 @@
 class HorsesController < ApplicationController
-  skip_before_filter :user_permission?, :only => [:index]
+  skip_before_filter :user_permission?, :only => [:index,:availability]
 
   def index
+    @horses = Horse.order("name")
+  end
+
+  def sort
+    # todo
   end
 
   def new
+    @horse = Horse.new
   end
 
   def edit
-  end
-
-  def show
-  end
-
-  def remove
-  end
-
-  def remove_info
+    # todo
   end
 
   def availability
-  end
-
-  def edit_availability
-  end
-
-  def set_availability
+    horse = Horse.find(params[:horse_id])
+    horse.availability = params[:value] == "true" ? true : false
+    horse.save
+    render :nothing => true
   end
 
   def create
+    # todo
   end
 
   def update
+    # todo
   end
 
   def destroy
+    # todo
   end
 end
