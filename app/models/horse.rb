5efeab2fbbbc9e9756021d582e94c3722436eb08
moliver777 <moliver@i-neda.com>
2012-09-27 09:49:33 +0100

    fixed weight comparison error

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -91,9 +91,9 @@ class Horse < ActiveRecord::Base
     Horse.all.each do |horse|
       issues << {:link => "/bookings/search/#{horse.id}", :text => horse.name+" is overworked today - Current workload: "+horse.workload(Date.today)+"hrs"} if horse.over_workload Date.today
       event_splits = []
-      horse.bookings.includes(:event).where("events.event_date = ?", Date.today).each do |booking|
-        issues << {:link => "/bookings/search/#{horse.id}", :text => horse.name+" is assigned to an over weight rider today at "+booking.event.start_time.strftime("%l:%M%P")+" - Max weight: #{horse.max_weight}st. Rider: #{booking.client.weight}st."} if booking.client.weight > horse.max_weight
-      end
+      # horse.bookings.includes(:event).where("events.event_date = ?", Date.today).each do |booking|
+      #   issues << {:link => "/bookings/search/#{horse.id}", :text => horse.name+" is assigned to an over weight rider today at "+booking.event.start_time.strftime("%l:%M%P")+" - Max weight: #{horse.max_weight}st. Rider: #{booking.client.weight}st."} if booking.client.weight > horse.max_weight
+      # end
       horse.events.where(:event_date => Date.today).each do |event|
         splits = []
         splits << event.start_time.strftime("%H:%M")
