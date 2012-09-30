f3a42aea0671aef6402b5df0faab51a28c77576b
moliver777 <moliver@i-neda.com>
2012-09-30 21:33:34 +0100

    session store fix

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -89,53 +89,57 @@ class Horse < ActiveRecord::Base
   def self.status
     issues = []
     Horse.all.each do |horse|
-      issues << {:link => "/bookings/search/#{horse.id}", :text => horse.name+" is overworked today - Current workload: "+horse.workload(Date.today)+"hrs"} if horse.over_workload Date.today
-      event_splits = []
-      horse.events.where(:event_date => Date.today).each do |event|
-        splits = []
-        splits << event.start_time.strftime("%H:%M")
-        hour = event.start_time.strftime("%H")
-        mins = event.start_time.strftime("%M")
-        while hour.to_s+":"+mins.to_s != event.end_time.strftime("%H:%M")
-          mins = mins.to_i+15
-          if mins == 60
-            mins = "00"
-            hour = hour.to_i+1
-            hour = "0"+hour.to_s if hour < 10
+      issues << {:link => "/bookings/search/#{horse.id}", :text => horse.name+" is overworked today - Current workload: "+horse.workload(Date.today)+"hrs"} if horse.over_workload Date.today      
+      date = Date.today
+      30.times do |i|
+        event_splits = []
+        horse.events.where(:event_date => date).each do |event|
+          splits = []
+          splits << event.start_time.strftime("%H:%M")
+          hour = event.start_time.strftime("%H")
+          mins = event.start_time.strftime("%M")
+          while hour.to_s+":"+mins.to_s != event.end_time.strftime("%H:%M")
+            mins = mins.to_i+15
+            if mins == 60
+              mins = "00"
+              hour = hour.to_i+1
+              hour = "0"+hour.to_s if hour < 10
+            end
+            splits << hour.to_s+":"+mins.to_s
           end
-          splits << hour.to_s+":"+mins.to_s
+          event_splits << splits
         end
-        event_splits << splits
-      end
-      # test for consecutive lessons (first of any splits array == last of any splits array)
-      event_splits.each_with_index do |event,i|
-        first = event.first
-        event_splits.each_with_index do |event2,j|
-          if i != j
-            issues << {:link => "/bookings/search/"+horse.id.to_s, :text => horse.name+" is booked into consecutive events with no break at "+first} if first == event2.last
-          end
-        end
-      end
-      # test for overlap of lessons
-      event_splits.each_with_index do |event,i|
-        event.each do |split|
+        # test for consecutive lessons (first of any splits array == last of any splits array)
+        event_splits.each_with_index do |event,i|
+          first = event.first
           event_splits.each_with_index do |event2,j|
-            event2.each do |split2|
-              if i != j && split2 != event2.first && split2 != event2.last
-                issues << {:link => "/bookings/search/"+horse.id.to_s, :text => horse.name+" is double-booked across different events"} if split == split2
+            if i != j
+              issues << {:link => "/bookings/date/#{date.strftime("%d-%m-%Y")}", :text => horse.name+" is booked into consecutive events with no break at "+first+(date==Date.today ? " today" : " on "+date.strftime("%A #{date.strftime("%e").to_i.ordinalize} %B"))} if first == event2.last
+            end
+          end
+        end
+        # test for overlap of lessons
+        event_splits.each_with_index do |event,i|
+          event.each do |split|
+            event_splits.each_with_index do |event2,j|
+              event2.each do |split2|
+                if i != j && split2 != event2.first && split2 != event2.last
+                  issues << {:link => "/bookings/date/#{date.strftime("%d-%m-%Y")}", :text => horse.name+" is double-booked across different events "+(date==Date.today ? " today" : " on "+date.strftime("%A #{date.strftime("%e").to_i.ordinalize} %B"))} if split == split2
+                end
               end
             end
           end
         end
-      end
-      # test for double booking
-      bookings = horse.bookings
-      bookings.each_with_index do |booking,i|
-        bookings.each_with_index do |booking2,j|
-          if i != j
-            issues << {:link => "/bookings/search/"+horse.id.to_s, :text => horse.name+" is double-booked for the "+booking.event.event_type.downcase.capitalize+" event at "+booking.event.start_time.strftime("%l:%M%P")} if booking.event == booking2.event
+        # test for double booking
+        bookings = horse.bookings
+        bookings.each_with_index do |booking,i|
+          bookings.each_with_index do |booking2,j|
+            if i != j
+              issues << {:link => "/bookings/date/#{date.strftime("%d-%m-%Y")}", :text => horse.name+" is double-booked for the "+booking.event.event_type.downcase.capitalize+" event at "+booking.event.start_time.strftime("%l:%M%P")+(date==Date.today ? " today" : " on "+date.strftime("%A #{date.strftime("%e").to_i.ordinalize} %B"))} if booking.event == booking2.event
+            end
           end
         end
+        date = date.advance(:days => 1)
       end
     end
     issues.uniq
