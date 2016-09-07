d2d54125f525b01ad1f59924e4e44ab0ea81ed98
Mark Oliver <moliver777@googlemail.com>
2016-09-07 16:26:56 +0100

    removed password locking of cash-up

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -410,8 +410,6 @@ class BookingsController < ApplicationController
   end
 
   def cash_up
-    @password = SiteSetting.where(:name => "cash_up_password").first.value rescue "password"
-    @bypass = (params[:bypass] && params[:bypass] == "true") ? true : false
     @date = params[:date] ? (Date.parse(params[:date]) rescue nil) : Date.today
     @totals = {"cash" => 0.00, "card" => 0.00, "cheque" => 0.00, "voucher" => 0.00, "hours" => 0.00, "foc" => 0.00, "total" => 0.00}
     @payments = Payment.where(:payment_date => @date).group_by{|p| p.friendly_type.downcase}
